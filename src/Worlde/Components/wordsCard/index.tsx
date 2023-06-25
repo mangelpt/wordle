3124d03cd416f2515416c3aa56851252569d7fac
Miguel Angel <mpalma@utp.edu.pe>
2023-06-25 16:32:43 -0500

    added proyect base

---
+++ src/Worlde/Components/wordsCard/index.tsx
@@ -0,0 +1,8 @@
+import {Container} from "./styled.ts";
+
+
+
+export const WordsCard = () => {
+
+    return <Container> G </Container>
+}
---
+++ src/Worlde/Components/wordsCard/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  width: 76px;
+  height: 76px;
+  flex-shrink: 0;
+  border-radius: 5px;
+  text-rendering: optimizeLegibility;
+`
